acdf34afdf1e4c978f8db230080ce484accc230a
marcos luciano <marcos3dt@gmail.com>
2024-09-06 19:26:21 -0300

    Trabalhando na resposividade do site

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  // Fechar o menu quando clicar fora
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (isOpen && !event.target.closest('.navbar')) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isOpen]);
+
   return (
     <header className="header">
       <nav className="navbar">
         <div className="navbar-container">
+          <div className="hamburger" onClick={toggleMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
           <Link to="/" className="logo">luciano.freire</Link>
-          
-          <ul className="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">Sobre Mim</Link></li>
-            <li><Link to="/portfolio">Portfólio</Link></li>
-            <li><Link to="/contact">Contato</Link></li>
+
+          <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+            <li onClick={closeMenu}><Link to="/">Home</Link></li>
+            <li onClick={closeMenu}><Link to="/about">Sobre Mim</Link></li>
+            <li onClick={closeMenu}><Link to="/portfolio">Portfólio</Link></li>
+            <li onClick={closeMenu}><Link to="/contact">Contato</Link></li>
           </ul>
         </div>
       </nav>
@@ -22,4 +52,3 @@ const Header = () => {
 };
 
 export default Header;
-    
